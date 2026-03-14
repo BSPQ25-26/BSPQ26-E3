e0851c3689870e377634f96e56aaa497f7e314bb
ineguiluz1 <i.eguiluz@opendeusto.es>
2026-03-14 16:42:47 +0100

    Test Pull Request

--- src/main/java/com/example/restapi/RestApiApplication.java
+++ src/main/java/com/example/restapi/RestApiApplication.java
@@ -9,6 +9,7 @@ public class RestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 		System.out.println("API is running...");
+		System.out.println("Confirmation");
 	}
 
 }
