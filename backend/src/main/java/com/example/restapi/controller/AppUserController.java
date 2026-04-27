81fc94e582d79954bbcbcd337f868a22b83fff11
Lydia <lbouaziz.ir2027@esaip.org>
2026-04-27 12:16:29 +0200

    fix: add pom.xml

--- backend/src/main/java/com/example/restapi/controller/AppUserController.java
+++ backend/src/main/java/com/example/restapi/controller/AppUserController.java
@@ -4,16 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.restapi.dto.AuthResponse;
 import com.example.restapi.dto.LoginRequest;
@@ -88,14 +81,14 @@ public class AppUserController {
         }
     }
 
-    @PutMapping("/{id}")
+    /*@PutMapping("/{id}")
     public ResponseEntity<Profile> updateUser(@PathVariable UUID id, @RequestBody Profile userDetails) {
         try {
             return ResponseEntity.ok(appUserService.updateUser(id, userDetails));
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
-    }
+    }*/
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
@@ -106,4 +99,14 @@ public class AppUserController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PatchMapping(value ="/{id}")
+    public ResponseEntity<Profile> updatePartOfUser(@RequestBody Map<String, Object> updates, @PathVariable UUID id){
+        try {
+            return ResponseEntity.ok(appUserService.updatePartOfUser(id, updates));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
