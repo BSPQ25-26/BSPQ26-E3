e5413040bcd1616413ee93fb5083caac8fe339b4
MateoPerezSuarez <mateo.perez.suarez@opendeusto.es>
2026-04-29 15:45:43 +0200

    newImplementationsErrorSolving

---
+++ backend/src/main/java/com/example/restapi/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.restapi.repository;
+
+import com.example.restapi.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
